056b1c83d58846d2049650c2e9c19c1e38bd2bb7
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-13 14:48:24 +0100

    Created Static Method For Calculate Average

---
+++ Task_02/Task_02/CalculateAverage.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_02;
+using System;
+
+public static class CalculateAverage
+{
+    public static double Average(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Array Can Not Be Null Or Empty.");
+        }
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        return (double)sum / numbers.Length;
+    }
+}
