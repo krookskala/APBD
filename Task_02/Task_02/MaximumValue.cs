f261e776dce5b5e5d73f590d8198bda9e1238573
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-13 15:00:52 +0100

    Added Static Method To Find Maximum Value

---
+++ Task_02/Task_02/MaximumValue.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_02;
+
+using System;
+
+public static class CalculateMax
+{
+    public static int FindMax(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Array Can Not Be Null Or Empty.");
+        }
+
+        int max = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+
+        return max;
+    }
+}
--- Task_02/Task_02/Program.cs
+++ Task_02/Task_02/Program.cs
@@ -9,3 +9,5 @@ Console.WriteLine("APBD_LAB_02");
 int[] numbers = { 7, 13, 31, 77, 100 };
 double average = CalculateAverage.Average(numbers);
 Console.WriteLine("Average: " + average);
+double maximum = CalculateMax.FindMax(numbers);
+Console.WriteLine("Maximum: " + maximum);
