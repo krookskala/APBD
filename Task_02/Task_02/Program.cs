38a75f97063b6800d54c47a84d2d411d59806731
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-13 14:49:15 +0100

    Created Static Method For Calculate Average

--- Task_02/Task_02/Program.cs
+++ Task_02/Task_02/Program.cs
@@ -1,6 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using Task_02;
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Ismail Sariarslan");
 Console.WriteLine("s22899");
 Console.WriteLine("APBD_LAB_02");
+
+int[] numbers = { 7, 13, 31, 77, 100 };
+double average = CalculateAverage.Average(numbers);
+Console.WriteLine("Average: " + average);
