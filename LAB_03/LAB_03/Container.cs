39dd68eaea7ef80c4672f96c34109bc00a1636d2
krookskala <ismailsariarslan7@gmail.com>
2024-03-15 16:48:24 +0100

    CREATED Container.cs

---
+++ LAB_03/LAB_03/Container.cs
@@ -0,0 +1,22 @@
+namespace LAB_03;
+
+public class Container
+{
+    public string SerialNumber { get; private set; }
+    public double CargoMass { get; private set; }
+    public double Height { get; private set; }
+    public double Depth { get; private set; }
+    public double TareWeight { get; private set; }
+    public double MaximumPayload { get; private set; }
+
+    public Container(string serialNumber, double cargoMass, double height, double depth, double tareWeight, double maximumPayload)
+    {
+        SerialNumber = serialNumber;
+        CargoMass = cargoMass;
+        Height = height;
+        Depth = depth;
+        TareWeight = tareWeight;
+        MaximumPayload = maximumPayload;
+    }
+        
+}
