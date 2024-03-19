d47b243257411617f5325dcd10eb7074a6bf5742
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-19 22:57:27 +0100

    CREATED LiquidContainer.cs

---
+++ LAB_03/LAB_03/LiquidContainer.cs
@@ -0,0 +1,10 @@
+﻿namespace LAB_03;
+
+public class LiquidContainer : Container
+{
+    public LiquidContainer(double maximumPayload, double tareWeight, double height, double depth) : base(maximumPayload,
+        tareWeight, height, depth)
+    {
+        
+    }
+}
