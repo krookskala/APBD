ab27f991dde49dac97b89b033452b18b65bca902
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-19 23:15:20 +0100

    CREATED GasContainer.cs

---
+++ LAB_03/LAB_03/GasContainer.cs
@@ -0,0 +1,23 @@
+﻿namespace LAB_03;
+using System;
+
+public class GasContainer : Container, HazardNotifier
+{
+    public double Pressure { get; private set; }
+
+    public GasContainer(double maximumPayload, double tareWeight, double height, double depth, double pressure) : base(
+        maximumPayload, tareWeight, height, depth)
+    {
+        Pressure = pressure;
+    }
+
+    public void Notify(string containerNumber)
+    {
+        Console.WriteLine($"Hazardous Situtation Detected In Gas Container {containerNumber}.");
+    }
+
+    public override void EmptyCargo()
+    {
+        CargoMass *= 0.05;
+    }
+}   
