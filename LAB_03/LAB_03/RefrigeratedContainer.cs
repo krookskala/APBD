3813929743fc84c3856bf7b40a49050b0045d7a1
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-19 22:54:58 +0100

    CREATED RefrigeratedContainer.cs

---
+++ LAB_03/LAB_03/RefrigeratedContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace LAB_03;
+
+public class RefrigeratedContainer : Container
+{
+    public string ProductType { get; private set; }
+    public double RequiredTemperature { get; private set; }
+
+    public RefrugeratedContainer(double maximumPayload, double tareWeight, double height, double depth,
+        string productType, double requiredTemperature) : base(maximumPayload, tareWeight, height, depth)
+    {
+        ProductType = productType;
+        RequiredTemperature = requiredTemperature;
+    }
+}
