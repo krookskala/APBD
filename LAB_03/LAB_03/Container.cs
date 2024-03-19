a0b0ea7c8f3b542ebcfecfbaa82f2796331b790c
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-19 22:54:42 +0100

    UPTADED Container.cs

--- LAB_03/LAB_03/Container.cs
+++ LAB_03/LAB_03/Container.cs
@@ -1,6 +1,6 @@
 namespace LAB_03;
 
-public class Container
+public abstract class Container
 {
     public string SerialNumber { get; private set; }
     public double CargoMass { get; private set; }
@@ -9,14 +9,29 @@ public class Container
     public double TareWeight { get; private set; }
     public double MaximumPayload { get; private set; }
 
-    public Container(string serialNumber, double cargoMass, double height, double depth, double tareWeight, double maximumPayload)
+    public Container(string serialNumber, double height, double depth, double tareWeight, double maximumPayload)
     {
         SerialNumber = serialNumber;
-        CargoMass = cargoMass;
         Height = height;
         Depth = depth;
         TareWeight = tareWeight;
         MaximumPayload = maximumPayload;
     }
-        
+
+    public LoadCargo(double cargoMass)
+    {
+        if (cargoMass > MaximumPayload)
+            throw new OverfillException();
+        CargoMass = cargoMass;
+    }
+
+    public void EmptyCargo()
+    {
+        CargoMass = 0;
+    }
+
+    protected string GenerateSerialNumber()
+    {
+        throw new NotImplementedException();
+    }
 }
