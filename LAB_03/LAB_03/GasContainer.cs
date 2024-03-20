bf10befa2e5d8d8f0ebe850388dc3d9e72f3c5eb
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-20 21:58:57 +0100

    UPTADED GasContainer.cs

--- LAB_03/LAB_03/GasContainer.cs
+++ LAB_03/LAB_03/GasContainer.cs
@@ -1,23 +1,31 @@
-﻿namespace LAB_03;
-using System;
-
-public class GasContainer : Container, HazardNotifier
+﻿namespace LAB_03
 {
-    public double Pressure { get; private set; }
-
-    public GasContainer(double maximumPayload, double tareWeight, double height, double depth, double pressure) : base(
-        maximumPayload, tareWeight, height, depth)
+    public class GasContainer : Container, IHazardNotifier
     {
-        Pressure = pressure;
-    }
+        public double Pressure { get; set; } 
+        public GasContainer(double cargoMass, int height, double tareWeight, int depth, string serialNumber, double maxPayload, double pressure)
+            : base(cargoMass, height, tareWeight, depth, serialNumber, maxPayload)
+        {
+            Pressure = pressure;
+        }
 
-    public void Notify(string containerNumber)
-    {
-        Console.WriteLine($"Hazardous Situtation Detected In Gas Container {containerNumber}.");
-    }
+        public override void LoadCargo(double mass)
+        {
+            if (mass > MaxPayload)
+            {
+                throw new OverfillException($"Attempting To Overfill Gas Container {SerialNumber}.");
+            }
+            CargoMass = mass;
+        }
 
-    public override void EmptyCargo()
-    {
-        CargoMass *= 0.05;
+        public override void EmptyCargo()
+        {
+            CargoMass *= 0.05;
+        }
+
+        public void NotifyHazard()
+        {
+            Console.WriteLine($"Hazardous Event Notified For Gas Container {SerialNumber}.");
+        }
     }
-}   
+}
