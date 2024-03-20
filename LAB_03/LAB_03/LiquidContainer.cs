305734ccf1bc9f79f60ea8a595e10394cdbd2cb7
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-20 22:00:55 +0100

    UPTADED LiquidContainer.cs

--- LAB_03/LAB_03/LiquidContainer.cs
+++ LAB_03/LAB_03/LiquidContainer.cs
@@ -1,10 +1,36 @@
-﻿namespace LAB_03;
-
-public class LiquidContainer : Container
+﻿namespace LAB_03
 {
-    public LiquidContainer(double maximumPayload, double tareWeight, double height, double depth) : base(maximumPayload,
-        tareWeight, height, depth)
+    public class LiquidContainer : Container, IHazardNotifier
     {
-        
+        public bool IsHazardous { get; set; }
+        public LiquidContainer(double cargoMass, int height, double tareWeight, int depth, string serialNumber, double maxPayload, bool isHazardous)
+            : base(cargoMass, height, tareWeight, depth, serialNumber, maxPayload)
+        {
+            IsHazardous = isHazardous;
+        }
+
+        public override void LoadCargo(double mass)
+        {
+            var maxLoad = IsHazardous ? MaxPayload * 0.5 : MaxPayload * 0.9;
+            if (mass > maxLoad)
+            {
+                NotifyHazard();
+                throw new OverfillException("Cargo Exceeds Maximum Allowable Payload.");
+            }
+            CargoMass = mass;
+        }
+
+        public override void EmptyCargo()
+        {
+            CargoMass = 0;
+        }
+
+        public void NotifyHazard()
+        {
+            if (IsHazardous)
+            {
+                Console.WriteLine($"Hazardous Cargo In Container {SerialNumber}.");
+            }
+        }
     }
 }
