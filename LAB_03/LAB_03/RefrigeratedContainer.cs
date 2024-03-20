1109f35640e9d6a945aca5308c19f3cf4cee9d95
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-20 22:05:32 +0100

    UPTADED RefrigeratedContainer.cs

--- LAB_03/LAB_03/RefrigeratedContainer.cs
+++ LAB_03/LAB_03/RefrigeratedContainer.cs
@@ -1,14 +1,58 @@
-﻿namespace LAB_03;
-
-public class RefrigeratedContainer : Container
+﻿namespace LAB_03
 {
-    public string ProductType { get; private set; }
-    public double RequiredTemperature { get; private set; }
-
-    public RefrugeratedContainer(double maximumPayload, double tareWeight, double height, double depth,
-        string productType, double requiredTemperature) : base(maximumPayload, tareWeight, height, depth)
+    public class RefrigeratedContainer : Container
     {
-        ProductType = productType;
-        RequiredTemperature = requiredTemperature;
+        public string ProductType { get; set; }
+        public double Temperature { get; set; }
+
+        public RefrigeratedContainer(double cargoMass, int height, double tareWeight, int depth, string serialNumber, double maxPayload, string productType, double temperature)
+            : base(cargoMass, height, tareWeight, depth, serialNumber, maxPayload)
+        {
+            ProductType = productType;
+            Temperature = temperature;
+        }
+
+        public override void LoadCargo(double mass)
+        {
+            if (mass > MaxPayload)
+            {
+                throw new OverfillException($"Attempting To Overfill Refrigerated Container {SerialNumber}.");
+            }
+
+            if (ProductTemperatureRequirements.Requirements.TryGetValue(ProductType, out var requiredTemp))
+            {
+                if (Temperature < requiredTemp.Min || Temperature > requiredTemp.Max)
+                {
+                    throw new ArgumentException($"{ProductType} Requires A Temperature Of {requiredTemp.Min} Degrees Celsius.");
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"No Temperature Requirements Defined For {ProductType}.");
+            }
+            CargoMass = mass;
+        }
+
+        public override void EmptyCargo()
+        {
+            CargoMass = 0;
+        }
+        
+        public static class ProductTemperatureRequirements
+        {
+            public static readonly Dictionary<string, (double Min, double Max)> Requirements = new Dictionary<string, (double Min, double Max)>
+            {
+                {"Bananas", (13.3, 13.3)},
+                {"Chocolate", (18, 18)},
+                {"Fish", (2, 2)},
+                {"Meat", (-15, -15)},
+                {"Ice Cream", (-18, -18)},
+                {"Frozen Pizza", (-30, -30)},
+                {"Cheese", (7.2, 7.2)},
+                {"Sausages", (5, 5)},
+                {"Butter", (20.5, 20.5)},
+                {"Eggs", (19, 19)}
+            };
+        }
     }
 }
