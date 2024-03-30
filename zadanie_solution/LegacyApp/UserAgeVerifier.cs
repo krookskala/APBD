df79ce452d2c719723bf8d44ce3bd9fd8521b83b
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-30 14:58:23 +0100

    ADDED UserAgeVerifier.cs

---
+++ zadanie_solution/LegacyApp/UserAgeVerifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserAgeVerifier : IUserValidator
+    {
+        public bool Validate(User user)
+        {
+            if (user == null || user.DateOfBirth == default)
+            {
+                return false;
+            }
+
+            var age = CalculateAge(user.DateOfBirth);
+            return age >= 21;
+        }
+
+        private int CalculateAge(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) 
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
