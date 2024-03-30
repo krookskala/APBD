7da4c35ec72e2e5c19fc3e6876686ef5866c8891
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-30 14:58:47 +0100

    ADDED UserEmailVerifier.cs

---
+++ zadanie_solution/LegacyApp/UserEmailVerifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    internal class UserEmailVerifier : IUserValidator
+    {
+        public bool Validate(User user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user), "User cannot be null.");
+            }
+
+            var email = user.EmailAddress;
+            return !string.IsNullOrEmpty(email) && email.Contains("@") && email.Contains(".");
+        }
+    }
+}
