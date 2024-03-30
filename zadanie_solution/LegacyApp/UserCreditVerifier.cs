b3149befb45747e6d5cbf64e6eaf4a437d04765a
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-30 14:58:34 +0100

    ADDED UserCreditService.cs

---
+++ zadanie_solution/LegacyApp/UserCreditVerifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    internal class UserCreditVerifier : IUserValidator
+    {
+        public bool Validate(User user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user), "User cannot be null.");
+            }
+
+            return !user.HasCreditLimit || user.CreditLimit >= 500;
+        }
+    }
+}
