fcd2aa16d460049fea947376d99d86928b75f3b7
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-16 19:44:50 +0200

    Added AddAnimal.cs

---
+++ LAB_06/VeterinaryClinicApi/VeterinaryClinicApi/Models/DTO/AddAnimal.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinicApi.Models.DTO;
+
+public class AddAnimal
+{
+    
+    [Required]
+    [MinLength(3)]
+    [MaxLength(200)]
+    public string? Name { get; set; }
+    
+    public string? Description { get; set; }
+    
+    [Required]
+    [MinLength(3)]
+    [MaxLength(200)]
+    public string? Category { get; set; }
+    
+    [Required]
+    [MinLength(5)]
+    [MaxLength(200)]
+    public string? Area { get; set; }
+}
