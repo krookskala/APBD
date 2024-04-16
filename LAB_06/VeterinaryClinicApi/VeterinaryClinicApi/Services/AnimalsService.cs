923996e51d54e1231929973d981922d34555d16f
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-16 19:44:37 +0200

    Added AnimalsService.cs

---
+++ LAB_06/VeterinaryClinicApi/VeterinaryClinicApi/Services/AnimalsService.cs
@@ -0,0 +1,40 @@
+﻿using VeterinaryClinicApi.Models;
+using VeterinaryClinicApi.Repositories;
+
+namespace VeterinaryClinicApi.Services
+{
+    public class AnimalsService : IAnimalsService
+    {
+        private readonly IAnimalsRepository _animalsRepository;
+
+        public AnimalsService(IAnimalsRepository animalsRepository)
+        {
+            _animalsRepository = animalsRepository;
+        }
+
+        public async Task<List<Animal>> GetAllAnimalsAsync(string orderBy = "Name")
+        {
+            return await _animalsRepository.GetAllAnimalsAsync(orderBy);
+        }
+
+        public async Task<Animal> GetAnimalByIdAsync(int id)
+        {
+            return await _animalsRepository.GetAnimalByIdAsync(id);
+        }
+
+        public async Task<Animal> AddAnimalAsync(Animal animal)
+        {
+            return await _animalsRepository.AddAnimalAsync(animal);
+        }
+
+        public async Task<bool> UpdateAnimalAsync(int id, Animal animal)
+        {
+            return await _animalsRepository.UpdateAnimalAsync(id, animal);
+        }
+
+        public async Task<bool> DeleteAnimalAsync(int id)
+        {
+            return await _animalsRepository.DeleteAnimalAsync(id);
+        }
+    }
+}
