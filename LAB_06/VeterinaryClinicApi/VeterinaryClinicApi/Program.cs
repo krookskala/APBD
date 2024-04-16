079ee182fa7d94cd851546ccf50a157beace6eb9
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-16 19:46:18 +0200

    Added Program.cs

---
+++ LAB_06/VeterinaryClinicApi/VeterinaryClinicApi/Program.cs
@@ -0,0 +1,27 @@
+using VeterinaryClinicApi.Repositories;
+using VeterinaryClinicApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddScoped<IAnimalsRepository, AnimalsRepository>();
+builder.Services.AddScoped<IAnimalsService, AnimalsService>();
+
+var app = builder.Build();
+
+app.Configuration.GetConnectionString("Default");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
