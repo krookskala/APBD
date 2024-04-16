cd01369eeb417c63ae0765e708e4ae7faccea65b
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-16 19:43:58 +0200

    Added Animal.cs

---
+++ LAB_06/VeterinaryClinicApi/VeterinaryClinicApi/Models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace VeterinaryClinicApi.Models;
+
+public class Animal
+{
+    public int IdAnimal { get; set; }
+    public string Name { get; set; } = string.Empty; 
+    public string? Description { get; set; } 
+    public string Category { get; set; } = string.Empty; 
+    public string Area { get; set; } = string.Empty; 
+}
