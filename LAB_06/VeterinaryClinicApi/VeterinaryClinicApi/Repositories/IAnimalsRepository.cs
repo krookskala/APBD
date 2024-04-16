b14f0e4dd493c5a82dbf01f035278e9a07037c21
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-16 19:45:01 +0200

    Added IAnimalsRepository.cs

---
+++ LAB_06/VeterinaryClinicApi/VeterinaryClinicApi/Repositories/IAnimalsRepository.cs
@@ -0,0 +1,13 @@
+﻿using VeterinaryClinicApi.Models;
+
+namespace VeterinaryClinicApi.Repositories
+{
+    public interface IAnimalsRepository
+    {
+        Task<List<Animal>> GetAllAnimalsAsync(string orderBy);
+        Task<Animal> GetAnimalByIdAsync(int id);
+        Task<Animal> AddAnimalAsync(Animal animal);
+        Task<bool> UpdateAnimalAsync(int id, Animal animal);
+        Task<bool> DeleteAnimalAsync(int id);
+    }
+}
