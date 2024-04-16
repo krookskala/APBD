7c84f72723b6ed81ec578c3009eafbc823250ebf
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-16 19:45:12 +0200

    Added IAnimalsService.cs

---
+++ LAB_06/VeterinaryClinicApi/VeterinaryClinicApi/Services/IAnimalsService.cs
@@ -0,0 +1,13 @@
+﻿using VeterinaryClinicApi.Models;
+
+namespace VeterinaryClinicApi.Services
+{
+    public interface IAnimalsService
+    {
+        Task<List<Animal>> GetAllAnimalsAsync(string orderBy = "Name");
+        Task<Animal> GetAnimalByIdAsync(int id);
+        Task<Animal> AddAnimalAsync(Animal animal);
+        Task<bool> UpdateAnimalAsync(int id, Animal animal);
+        Task<bool> DeleteAnimalAsync(int id);
+    }
+}
