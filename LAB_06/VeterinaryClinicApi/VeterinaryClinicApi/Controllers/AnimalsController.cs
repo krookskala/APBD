ad0a8ae848c5f203df5412926414f63aca6e6692
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-16 19:44:17 +0200

    Added AnimalsController.cs

---
+++ LAB_06/VeterinaryClinicApi/VeterinaryClinicApi/Controllers/AnimalsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VeterinaryClinicApi.Models;
+using VeterinaryClinicApi.Services;
+
+namespace VeterinaryClinicApi.Controllers
+{
+    [ApiController]
+    [Route("api/animals")]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly IAnimalsService _animalsService;
+
+        public AnimalsController(IAnimalsService animalsService)
+        {
+            _animalsService = animalsService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAnimals([FromQuery] string orderBy = "Name")
+        {
+            var animals = await _animalsService.GetAllAnimalsAsync(orderBy);
+            return Ok(animals);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAnimalById(int id)
+        {
+            var animal = await _animalsService.GetAnimalByIdAsync(id);
+            if (animal == null)
+                return NotFound($"Animal with ID {id} is not found");
+            return Ok(animal);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAnimal([FromBody] Animal animal)
+        {
+            var addedAnimal = await _animalsService.AddAnimalAsync(animal);
+            return CreatedAtAction(nameof(GetAnimalById), new { id = addedAnimal.IdAnimal }, addedAnimal);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAnimal(int id, [FromBody] Animal animal)
+        {
+            var existingAnimal = await _animalsService.GetAnimalByIdAsync(id);
+            if (existingAnimal == null)
+            {
+                var addedAnimal = await _animalsService.AddAnimalAsync(animal);
+                return CreatedAtAction(nameof(GetAnimalById), new { id = addedAnimal.IdAnimal }, addedAnimal);
+            }
+            else
+            {
+                await _animalsService.UpdateAnimalAsync(id, animal);
+                return NoContent(); 
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAnimal(int id)
+        {
+            var deleted = await _animalsService.DeleteAnimalAsync(id);
+            if (!deleted)
+                return NotFound($"Animal with ID {id} is not found");
+
+            return NoContent();
+        }
+    }
+}
