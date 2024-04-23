2710a74ca55c0536c0f27fde1f65a6aa4e5a9fe3
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-24 01:26:38 +0200

    Added WarehouseEntity.cs

---
+++ LAB_07/Warehouse/src/Warehouse/Entity/WarehouseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Warehouse.Warehouse.Entity 
+{
+    public class WarehouseEntity
+    {
+        public int IdWarehouse { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
