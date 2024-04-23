88360685693e41418593ea0d6824ad0a726e2b05
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-24 01:25:32 +0200

    Added ProductRepository.cs

---
+++ LAB_07/Warehouse/src/Product/Repository/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Warehouse.Product.Entity;
+using Warehouse.Product.Interface;
+
+namespace Warehouse.Product.Repository;
+
+public class ProductRepository : IProductRepository
+{
+    private IConfiguration _configuration;
+    
+    public ProductRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public ProductEntity? Get(int id)
+    {
+        var con = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        con.Open();
+        
+        using var cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = "SELECT IdProduct, Name, Description, Price FROM Product WHERE IdProduct = @Id";
+        cmd.Parameters.AddWithValue("@Id", id);
+        
+        var dr = cmd.ExecuteReader();
+        
+        ProductEntity product = null;
+        if (dr.Read())
+        {
+            product = new ProductEntity
+            {
+                Id = (int)dr["IdProduct"],
+                Name = dr["Name"].ToString(),
+                Description = dr["Description"].ToString(),
+                Price = Convert.ToDouble(dr["Price"])
+            };
+        }
+        
+        con.Close();
+        
+        return product;
+    }
+}
