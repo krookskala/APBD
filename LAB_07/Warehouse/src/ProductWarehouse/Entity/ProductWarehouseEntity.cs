a15ccdeceabc039c37bea598f36acd099d2dd960
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-24 01:25:52 +0200

    Added ProductWarehouseEntity.cs

---
+++ LAB_07/Warehouse/src/ProductWarehouse/Entity/ProductWarehouseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Warehouse.ProductWarehouse.Entity
+{
+    public class ProductWarehouseEntity
+    {
+        int IdProductWarehouse { get; set; }
+        int IdWarehouse { get; set; }
+        int IdProduct { get; set; }
+        int IdOrder { get; set; }
+        int Amount { get; set; }
+        double Price { get; set; }
+        DateTime CreatedAt { get; set; }
+    }
+}
