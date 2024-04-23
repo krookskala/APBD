e5ca4dbd676c088fa6efeca450d40df79c7e380e
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-24 01:25:08 +0200

    Added IWarehouseRepository.cs

---
+++ LAB_07/Warehouse/src/Warehouse/Interface/IWarehouseRepository.cs
@@ -0,0 +1,9 @@
+﻿using Warehouse.Warehouse.Entity;
+
+namespace Warehouse.Warehouse.Interface
+{
+    public interface IWarehouseRepository
+    {
+        WarehouseEntity Get(long idWarehouse);
+    }
+}
