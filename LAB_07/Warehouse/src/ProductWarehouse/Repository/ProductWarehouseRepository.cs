d89153a3c14f6b727149cf56078e01ce3a631ac5
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-24 01:26:01 +0200

    Added ProductWarehouseRepository.cs

---
+++ LAB_07/Warehouse/src/ProductWarehouse/Repository/ProductWarehouseRepository.cs
@@ -0,0 +1,12 @@
+﻿using Warehouse.ProductWarehouse.Entity;
+using Warehouse.ProductWarehouse.Interface;
+
+namespace Warehouse.ProductWarehouse.Repository;
+
+public class ProductWarehouseRepository : IProductWarehouseRepository
+{
+    public ProductWarehouseEntity SaveProduct(ProductWarehouseEntity productWarehouseEntity)
+    {
+        throw new NotImplementedException();
+    }
+}
