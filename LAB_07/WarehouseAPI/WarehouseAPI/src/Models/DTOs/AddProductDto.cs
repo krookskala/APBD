f4dfae537bb303d3633262c0bed6d7a019e6fc21
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-25 16:56:13 +0200

    Added AddProductDto.cs

---
+++ LAB_07/WarehouseAPI/WarehouseAPI/src/Models/DTOs/AddProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseAPI.Models.DTOs
+{
+    public class AddProductDto
+    {
+        [Required]
+        [MaxLength(200)] 
+        public string Name { get; set; }
+        
+        [Required]
+        [MaxLength(200)] 
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive number.")]
+        public decimal Price { get; set; }
+    }
+}
