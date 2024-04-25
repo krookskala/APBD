baffca0156ddc0ad72cc09a7f2de9d7ff490acca
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-25 17:01:53 +0200

    Added Order.cs

---
+++ LAB_07/WarehouseAPI/WarehouseAPI/src/Models/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace WarehouseAPI.Models.Entities
+{
+    public class Order
+    {
+        int IdOrder { get; set; }
+        int IdProduct { get; set; }
+        int Amount { get; set; }
+        DateTime CreatedAt { get; set; }
+        DateTime FulfilledAt { get; set; }
+    }
+}
