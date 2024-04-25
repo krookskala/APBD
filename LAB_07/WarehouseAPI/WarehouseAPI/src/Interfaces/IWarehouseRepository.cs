782a806e3e951e4d7cfa0c586959742ca3bea31b
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-25 17:02:57 +0200

    Added IWarehouseRepository.cs

---
+++ LAB_07/WarehouseAPI/WarehouseAPI/src/Interfaces/IWarehouseRepository.cs
@@ -0,0 +1,17 @@
+﻿using WarehouseAPI.Models.DTOs;
+
+namespace WarehouseAPI.Interfaces
+{
+    public interface IWarehouseRepository
+    {
+        Task<bool> DoesProductExists(int id);
+        Task<bool> DoesWarehouseExists(int id);
+        Task<bool> DoesOrderExists(int id, int amount, DateTime created);
+        Task<int> GetOrderId(int id);
+        Task<bool> WasOrderRealized(int id);
+        Task UpdateDate(int id);
+        Task<double> GetPrice(int id);
+        Task<int> InsertIntoProduct_Warehouse(InsertProductDto data, double cena, int id);
+        Task<int> InsertIntoProduct_Warehouse_With_Procedure(InsertProductDto data);
+    }
+}
