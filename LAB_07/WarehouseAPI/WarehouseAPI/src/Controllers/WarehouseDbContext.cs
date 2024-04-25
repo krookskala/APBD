ac7e57185c4941b5f6db2412f3f1a85455969248
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-25 16:59:58 +0200

    Added WarehouseDbContext.cs

---
+++ LAB_07/WarehouseAPI/WarehouseAPI/src/Controllers/WarehouseDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseAPI.Models.Entities;
+
+namespace WarehouseAPI.Controllers
+{
+    public class WarehouseDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ProductWarehouse> ProductWarehouses { get; set; }
+
+        public WarehouseDbContext(DbContextOptions<WarehouseDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().ToTable("Product");
+            modelBuilder.Entity<Warehouse>().ToTable("Warehouse");
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<ProductWarehouse>().ToTable("Product_Warehouse");
+        }
+    }
+}
