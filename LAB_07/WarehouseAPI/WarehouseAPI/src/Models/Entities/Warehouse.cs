8804369a29af3429b521e60da12be12bc7178c49
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-04-25 17:01:17 +0200

    Added Warehouse.cs

---
+++ LAB_07/WarehouseAPI/WarehouseAPI/src/Models/Entities/Warehouse.cs
@@ -0,0 +1,9 @@
+﻿namespace WarehouseAPI.Models.Entities
+{
+    public class Warehouse
+    {
+        private int IdWarehouse { get; set; }
+        string Name { get; set; }
+        string Address { get; set; }
+    }
+}
