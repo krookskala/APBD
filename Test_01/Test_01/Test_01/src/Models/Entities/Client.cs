ea9d5546544d854c06cbde26fc90bb4506d94d5f
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-05-10 17:28:37 +0200

    Added

---
+++ Test_01/Test_01/Test_01/src/Models/Entities/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace Test_01.Repositories
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? City { get; set; } = string.Empty;
+        public string? MobileNumber { get; set; }
+    }
+}
