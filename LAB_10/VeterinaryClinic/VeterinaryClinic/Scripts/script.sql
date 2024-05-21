0ea7e8db652b4fc757fe2fade1f0222b9f6df20b
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-05-21 22:03:51 +0200

    Added script.sql

---
+++ LAB_10/VeterinaryClinic/VeterinaryClinic/Scripts/script.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Animal (
+    Id INT PRIMARY KEY IDENTITY,
+    Name VARCHAR(200) NOT NULL,
+    Description VARCHAR(200),
+    Category VARCHAR(200) NOT NULL,
+    Area VARCHAR(200) NOT NULL 
+);
