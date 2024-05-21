850c1770564b909fc2ed7f1b1dc55e2b42ccd4e9
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-05-21 22:03:09 +0200

    Added AnimalContext.cs

---
+++ LAB_10/VeterinaryClinic/VeterinaryClinic/src/Services/AnimalContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VeterinaryClinic.Models;
+
+namespace VeterinaryClinic.Services
+{
+    public class AnimalContext : DbContext
+    {
+        
+        public AnimalContext()
+        {
+            
+        }
+        
+        public AnimalContext(DbContextOptions<AnimalContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Animal>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.ToTable("Animal");
+                entity.Property(e => e.Name).HasMaxLength(200);
+                entity.Property(e => e.Category).HasMaxLength(200);
+                entity.Property(e => e.Description).HasMaxLength(200);
+                entity.Property(e => e.Area).HasMaxLength(200);
+            });
+        }
+    }
+}
