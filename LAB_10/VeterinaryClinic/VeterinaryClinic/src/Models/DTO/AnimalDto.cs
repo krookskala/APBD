ed8cf5f76fe317b5ce57b1befb9a2319fc967825
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-05-21 22:03:28 +0200

    Added AnimalDto.cs

---
+++ LAB_10/VeterinaryClinic/VeterinaryClinic/src/Models/DTO/AnimalDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.Models.DTO
+{
+    public class AnimalDto
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
+        public required string Name { get; set; }
+        
+        [MinLength(0)]
+        [MaxLength(200)]
+        public required string Description { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
+        public required string Category { get; set; }
+        
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
+        public required string Area { get; set; }
+    }
+}
