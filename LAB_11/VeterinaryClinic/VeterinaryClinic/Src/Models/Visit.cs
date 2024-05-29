307e7c8638ac78096ec036c42c1dc8a404a03b97
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-05-29 21:23:39 +0200

    Added Visit.cs

---
+++ LAB_11/VeterinaryClinic/VeterinaryClinic/Src/Models/Visit.cs
@@ -0,0 +1,13 @@
+﻿namespace VeterinaryClinic.Models
+{
+    public class Visit
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; } = null!;
+        public int AnimalId { get; set; }
+        public Animal Animal { get; set; } = null!;
+        public DateTime Date { get; set; }
+        public byte[] RowVersion { get; set; } = null!;
+    }
+}
