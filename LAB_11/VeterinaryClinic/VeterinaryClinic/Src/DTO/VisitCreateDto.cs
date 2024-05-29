23dfb9660ded61234e24c08adf1cd14922383393
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-05-29 21:24:25 +0200

    Added VisitCreateDto.cs

---
+++ LAB_11/VeterinaryClinic/VeterinaryClinic/Src/DTO/VisitCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Build.Framework;
+
+namespace VeterinaryClinic.DTO
+{
+    public class VisitCreateDto
+    {
+        [Required] 
+        public int EmployeeId { get; set; }
+
+        [Required] 
+        public int AnimalId { get; set; }
+
+        [Required] 
+        public DateTime Date { get; set; }
+    }
+}
