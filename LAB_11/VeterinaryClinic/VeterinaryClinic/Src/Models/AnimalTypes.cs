e9aed4003005d46bdfe8cb6171f0896ec919f779
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-05-31 21:37:44 +0200

    Modified

--- LAB_11/VeterinaryClinic/VeterinaryClinic/Src/Models/AnimalTypes.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VeterinaryClinic.Models
-{
-    public class AnimalTypes
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public ICollection<Animal> Animals { get; set; } = new List<Animal>();
-    }
-}
