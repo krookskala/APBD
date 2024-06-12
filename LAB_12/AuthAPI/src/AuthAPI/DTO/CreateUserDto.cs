883fbd7071855902db4a7740a40b5fdf80ca54fb
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 00:23:48 +0200

    Added CreateUserDto.cs

---
+++ LAB_12/AuthAPI/src/AuthAPI/DTO/CreateUserDto.cs
@@ -0,0 +1,22 @@
+﻿namespace AuthAPI
+{
+    public class CreateUserDto
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 8)]
+        public required string Username { get; set; }
+        
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
+        public required string Password { get; set; }
+
+        public User Map()
+        {
+            return new User
+            {
+                Username = Username,
+                Password = Password
+            };
+        }
+    }
+}
