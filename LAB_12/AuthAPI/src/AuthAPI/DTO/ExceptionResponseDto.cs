1d0e5089a4f362a5294175b84d8207917f200f3f
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 00:23:26 +0200

    Added ExceptionResponseDto.cs

---
+++ LAB_12/AuthAPI/src/AuthAPI/DTO/ExceptionResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DefaultNamespace
+{
+    public class ExceptionResponseDto
+    {
+        public HttpStatusCode StatusCode { get; }
+        public string Message { get; }
+
+        public ExceptionResponseDto(HttpStatusCode statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+    }
+}
