6822a8587499238a03656bb85cc13eab2136ed11
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 00:22:58 +0200

    Added IUserService.cs

---
+++ LAB_12/AuthAPI/src/AuthAPI/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthAPI
+{
+    public class IUserService
+    {
+        Task<AuthDto> RegisterUserAsync(CreateUserDto createUserDto);
+        Task<AuthDto> AuthenticateUserAsync(LoginDto loginDto);
+        Task<AuthDto> RefreshTokenAsync(string refreshToken);
+    }
+}
