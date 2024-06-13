401b35ec5a4e9d079c6e2f19d9ca2371af8967f4
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:04:55 +0200

    Added Employee.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 1)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(20, MinimumLength = 1)]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        [StringLength(200, MinimumLength = 1)]
+        public string Email { get; set; } = null!;
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; } = null!;
+    }
+}
