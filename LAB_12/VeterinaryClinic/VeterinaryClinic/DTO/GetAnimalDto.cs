63a80420959c7db35deea9ced62d478c659e2528
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:05:27 +0200

    Added GetAnimalDto.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/DTO/GetAnimalDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.DTO
+{
+    public class GetAnimalDto
+    {
+        [Required(ErrorMessage = "The AnimalId Field Is Required.")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name Field Is Required.")]
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "The AnimalType Field Is Required.")]
+        public string AnimalType { get; set; } = null!;
+    }
+}
