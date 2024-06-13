d2cba3462af4a76b6c6884731d227287d8b71e3b
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:05:38 +0200

    Added GetVisitDto.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/DTO/GetVisitDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.DTO
+{
+    public class GetVisitDto
+    {
+        [Required(ErrorMessage = "The EmployeeId Field Is Required.")]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "The AnimalId Field Is Required.")]
+        public int AnimalId { get; set; }
+
+        [Required(ErrorMessage = "The Date Field Is Required.")]
+        public DateTime Date { get; set; }
+    }
+}
