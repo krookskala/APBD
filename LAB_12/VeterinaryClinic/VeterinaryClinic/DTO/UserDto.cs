b599597c5a1d7a228d531fb7de10c845336f621b
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:07:07 +0200

    Added UserDto.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/DTO/UserDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VeterinaryClinic.Models;
+
+namespace VeterinaryClinic.DTO;
+
+public class UserDto
+{
+    [Required(ErrorMessage = "The Username Is Required.")]
+    [StringLength(50, MinimumLength = 8, ErrorMessage = "Username Should Be Between 8 And 50 Characters!")]
+    public required string Username { get; set; }
+        
+    [Required(ErrorMessage = "The Password Is Required.")]
+    [StringLength(50, MinimumLength = 8, ErrorMessage = "Password Should Be Between 8 And 50 Characters!")]
+    public required string Password { get; set; }
+
+    public User Map()
+    {
+        return new User
+        {
+            Username = Username,
+            Password = Password
+        };
+    }
+}
