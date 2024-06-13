1830383b1f2536a9af3d945f7b980432981fecf0
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:08:45 +0200

    Added LoginUserDto.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/DTO/LoginUserDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.DTO;
+
+public class LoginUserDto
+{
+    [Required]
+    public required string Username { get; set; }
+        
+    [Required]
+    public required string Password { get; set; }
+}
