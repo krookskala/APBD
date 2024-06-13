3749a7d52c92439aee16dc85ce8078651818576c
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:03:14 +0200

    Added AddVisitDto.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/DTO/AddVisitDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.DTO
+{
+    public class AddVisitDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string EmployeeName { get; set; } = null!;
+
+        [Required]
+        public string AnimalName { get; set; } = null!;
+
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
