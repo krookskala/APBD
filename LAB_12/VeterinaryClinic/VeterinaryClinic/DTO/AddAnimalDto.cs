72dec4d62173d57e5170dd4574706ec1d07245e4
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:03:01 +0200

    Added AddAnimalDto.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/DTO/AddAnimalDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.DTO
+{
+    public class AddAnimalDto
+    {
+        [Required(ErrorMessage = "The Name Field Is Required.")]
+        [MaxLength(100, ErrorMessage = "The Name Field Can Contain A Maximum Of 100 Characters.")]
+        public required string Name { get; set; } 
+
+        [MaxLength(2000, ErrorMessage = "The Description Field Can Contain A Maximum Of 2000 Characters.")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "The AnimalType Field Is Required.")]
+        public required string AnimalType { get; set; } 
+    }
+}
