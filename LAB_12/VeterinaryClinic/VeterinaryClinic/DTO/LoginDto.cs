cbe659e4c90abbc19de5749749b0dbe9136ebfe7
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:08:56 +0200

    Added LoginDto.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/DTO/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace VeterinaryClinic.DTO
+{
+    public class LoginDto
+    {
+        public required string AccessToken { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
