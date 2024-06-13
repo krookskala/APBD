0e7fc1adba574695a4aa4f3a69a574a1971d88a0
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:06:42 +0200

    Added UserRole.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/Models/UserRole.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.Models;
+
+public class UserRole
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "The Name Field Is Required.")]
+    [StringLength(50, MinimumLength = 8, ErrorMessage = "Name Should Be Between 8 And 50 Characters!")]
+    public required string Name { get; set; }
+}
