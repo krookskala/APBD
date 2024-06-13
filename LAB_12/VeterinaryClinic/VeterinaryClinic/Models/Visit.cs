e71977d550d94abd86b81659f9dd9d828b4eea88
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:06:23 +0200

    Added Visit.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/Models/Visit.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VeterinaryClinic.Models
+{
+    public class Visit
+    {
+        public int Id { get; set; }
+
+        [Required][ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; } = null!;
+
+        [Required][ForeignKey("Animal")]
+        public int AnimalId { get; set; }
+        public Animal Animal { get; set; } = null!;
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; } = null!;
+    }
+}
