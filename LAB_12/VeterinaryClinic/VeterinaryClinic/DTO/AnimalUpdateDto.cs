06cab117d29107aa54ed3cc9a7e516b590a236f9
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:04:08 +0200

    Added AnimalUpdateDto.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/DTO/AnimalUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.DTO
+{
+    public class AnimalUpdateDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(2000)]
+        public string? Description { get; set; }
+    }
+}
