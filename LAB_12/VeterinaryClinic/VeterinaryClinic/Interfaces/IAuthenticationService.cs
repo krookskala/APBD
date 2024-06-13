d29834b549e5ec7f8a39cf94e5c74170476452c0
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:09:08 +0200

    Added IAuthenticationService.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using VeterinaryClinic.Models;
+
+namespace VeterinaryClinic.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        string GenerateAccessToken(User user);
+        string GenerateRefreshToken();
+        Task<bool> ValidateExpiredTokenAsync(string accessToken);
+    }
+}
