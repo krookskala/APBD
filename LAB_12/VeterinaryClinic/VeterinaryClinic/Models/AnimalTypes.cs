25264e6251dc4db80e32ac08906f53903c3b1aac
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-13 04:03:58 +0200

    Added AnimalTypes.cs

---
+++ LAB_12/VeterinaryClinic/VeterinaryClinic/Models/AnimalTypes.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.Models
+{
+    public class AnimalTypes
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150, MinimumLength = 1)]
+        public string Name { get; set; } = null!;
+        public ICollection<Animal> Animals { get; set; } = new List<Animal>();
+    }
+}
