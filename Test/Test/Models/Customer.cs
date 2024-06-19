dd60be7793f78f80bfcd3debbb88e35e4c8cd270
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-19 16:37:09 +0200

    Added Customer.cs

---
+++ Test/Test/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace Test.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string Country { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+
+    public ICollection<Order> Orders { get; set; } = null!;
+}
