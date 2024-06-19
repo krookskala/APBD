f0ecda49aeca0b2ac903c72104db70f4dacbfee5
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-19 16:37:46 +0200

    Added Supplier.cs

---
+++ Test/Test/Models/Supplier.cs
@@ -0,0 +1,14 @@
+﻿namespace Test.Models;
+
+public class Supplier
+{
+    public int Id { get; set; }
+    public string CompanyName { get; set; } = null!;
+    public string ContactName { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string Country { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+    public string Fax { get; set; } = null!;
+
+    public ICollection<Product> Products { get; set; } = null!;
+}
