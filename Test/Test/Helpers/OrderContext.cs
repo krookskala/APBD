127d739c794fc55471d70622cfbe288c3de69a9f
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-19 16:36:28 +0200

    Added OrderContext.cs

---
+++ Test/Test/Helpers/OrderContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test.Models;
+
+namespace Test.Helpers;
+
+public class OrderContext : DbContext
+{
+    public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>().HasKey(c => c.Id);
+        modelBuilder.Entity<Order>().HasKey(o => o.Id);
+        modelBuilder.Entity<OrderItem>().HasKey(oi => oi.Id);
+        modelBuilder.Entity<Product>().HasKey(p => p.Id);
+        modelBuilder.Entity<Supplier>().HasKey(s => s.Id);
+
+        modelBuilder.Entity<Order>().HasOne(o => o.Customer)
+            .WithMany(c => c.Orders)
+            .HasForeignKey(o => o.CustomerId);
+
+        modelBuilder.Entity<OrderItem>().HasOne(oi => oi.Order)
+            .WithMany(o => o.OrderItems)
+            .HasForeignKey(oi => oi.OrderId);
+
+        modelBuilder.Entity<OrderItem>().HasOne(oi => oi.Product)
+            .WithMany(p => p.OrderItems)
+            .HasForeignKey(oi => oi.ProductId);
+
+        modelBuilder.Entity<Product>().HasOne(p => p.Supplier)
+            .WithMany(s => s.Products)
+            .HasForeignKey(p => p.SupplierId);
+    }
+}
