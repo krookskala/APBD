29b6bcb73b49e01ee23cb5c5b84a4159762ced89
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-19 16:36:06 +0200

    Added OrderCreationDto.cs

---
+++ Test/Test/DTO/OrderCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Test.DTO;
+
+public class OrderCreationDto
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+
+    public List<ProductDto> Items { get; set; } = null!;
+}
