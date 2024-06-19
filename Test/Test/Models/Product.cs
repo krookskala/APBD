46c76a0abf722381d3c8962611c66d6725aeb400
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-19 16:37:37 +0200

    Added Product.cs

---
+++ Test/Test/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Test.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; } = null!;
+    public Supplier Supplier { get; set; } = null!;
+    public int SupplierId { get; set; }
+    public decimal UnitPrice { get; set; }
+    public string Package { get; set; } = null!;
+    public bool IsDiscontinued { get; set; }
+
+    public ICollection<OrderItem> OrderItems { get; set; } = null!;
+}
