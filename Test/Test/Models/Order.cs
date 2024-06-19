c1ee9f1acaf879fa96d067399fe42e3baffce972
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-06-19 16:37:18 +0200

    Added Order.cs

---
+++ Test/Test/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public DateTime OrderDate { get; set; }
+    public Customer Customer { get; set; }
+    public int CustomerId { get; set; }
+    public decimal TotalAmount { get; set; }
+
+    public ICollection<OrderItem> OrderItems { get; set; } = null!;
+    
+    [ConcurrencyCheck]
+    public string ConcurrencyToken { get; set; } = Guid.NewGuid().ToString(); 
+}
